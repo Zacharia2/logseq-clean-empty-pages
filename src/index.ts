fe3b25aec9a5024ece1c94e465b77af7f89d638f
Zacharia2 <32425955+Zacharia2@users.noreply.github.com>
2024-03-21 20:50:25 +0800

    up
    up

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,68 @@
 import "@logseq/libs";
 import { BlockEntity } from "@logseq/libs/dist/LSPlugin";
+import { LSPluginUserEvents } from "@logseq/libs/dist/LSPlugin.user";
+// @ts-expect-error
+const css = (t, ...args) => String.raw(t, ...args);
 
 async function main() {
 
-    /**
-     * 清理空白日志
-     * @param params 
-     */
-    async function clean_blank_journal(params: string) {
-        // deletePage: ((pageName: string) => Promise<void>)
-        // getAllPages: ((repo?: string) => Promise<PageEntity[]>)
-        // getPagesFromNamespace: ((namespace: string) => Promise<PageEntity[]>)
-        // 查找空白日志
-        // 验证是否为空白日志
+  /**
+   * 清理空白日志
+   * @param params 
+   */
+  async function clean_blank_journal(params: string) {
+    let a = await logseq.Editor.getAllPages()
+    // deletePage: ((pageName: string) => Promise<void>)
+    // getAllPages: ((repo?: string) => Promise<PageEntity[]>)
+    // getPagesFromNamespace: ((namespace: string) => Promise<PageEntity[]>)
+    // 查找空白日志
+    // 验证是否为空白日志
+    // {:block/uuid #uuid "65fc118b-8f27-4269-9a88-9b48d5f99230",
+    // :block/journal? true,
+    // :block/updated-at 1711018379197,
+    // :block/created-at 1711018379197,
+    // :block/journal-day 20240321,
+    // :block/format :markdown,
+    // :db/id 1468,
+    // :block/name "mar 21st, 2024",
+    // :block/file {:db/id 1474}, 
+    // :block/original-name "Mar 21st, 2024"}
 
+
+  }
+
+  const openIconName = "clean-empty-pages";
+
+  logseq.provideStyle(css`
+    .${openIconName} {
+      opacity: 0.55;
+      font-size: 20px;
+      margin-top: 4px;
     }
 
-    logseq.beforeunload(
-        logseq.App.registerPageMenuItem('clean empty pages', async ({ page }) => {
+    .${openIconName}:hover {
+      opacity: 0.9;
+    }
+  `);
 
-            // `logseq.Editor.getPage(page, { includeChildren: true });`
-            // will return a null children
-
-            const blocks = await logseq.Editor.getPageBlocksTree(page);
-            if (blocks.length) {
-                for (const block of blocks) {
-                    // if (await walk(block)) {
-                    //     await logseq.Editor.removeBlock(block.uuid);
-                    // }
-                }
-            }
-        }) as any
-    );
+  logseq.App.registerUIItem("toolbar", {
+    key: openIconName,
+    template: `
+        <div data-on-click="show" class="${openIconName}">⚙️</div>
+    `,
+  });
 }
 
 if (typeof logseq !== 'undefined') {
-    logseq.ready(main).catch(console.error);
+  logseq.ready(main).catch(console.error);
+}
+
+function subscribeLogseqEvent<T extends LSPluginUserEvents>(
+  eventName: T,
+  handler: (...args: any) => void
+) {
+  logseq.on(eventName, handler);
+  return () => {
+    logseq.off(eventName, handler);
+  };
 }
--- src/test.ts
+++ src/test.ts
@@ -2,60 +2,47 @@ import "@logseq/libs";
 import { BlockEntity } from "@logseq/libs/dist/LSPlugin";
 
 async function main() {
-
-    /**
-     * 清理空白日志
-     * @param params 
-     */
-    async function clean_blank_journal(params: string) {
-        // deletePage: ((pageName: string) => Promise<void>)
-        // getAllPages: ((repo?: string) => Promise<PageEntity[]>)
-        // getPagesFromNamespace: ((namespace: string) => Promise<PageEntity[]>)
-        // 查找空白日志
-        // 验证是否为空白日志
-
-    }
     /**
      * Returns true if the block tree is empty,
      * won't remove itself
      */
-    // async function walk(block: BlockEntity) {
-    //     if (!block.children) {
-    //         // not loaded
-    //         return false;
-    //     }
+    async function walk(block: BlockEntity) {
+        if (!block.children) {
+            // not loaded
+            return false;
+        }
 
-    //     const blocks: Array<{
-    //         block: BlockEntity;
-    //         result: boolean
-    //     }> = [];
+        const blocks: Array<{
+            block: BlockEntity;
+            result: boolean
+        }> = [];
 
-    //     for (const subBlock of block.children) {
-    //         blocks.push({
-    //             block: <BlockEntity> subBlock,
-    //             result: await walk(<BlockEntity> subBlock)
-    //         })
-    //     }
+        for (const subBlock of block.children) {
+            blocks.push({
+                block: <BlockEntity>subBlock,
+                result: await walk(<BlockEntity>subBlock)
+            })
+        }
 
-    //     if (blocks.every(x => x.result) && block.content === '') {
-    //         return true; // need caller to remove
-    //     }
-    //     else {
-    //         for (const subBlock of blocks.filter(x => x.result).map(x => x.block)) {
-    //             await logseq.Editor.removeBlock(subBlock.uuid);
-    //         }
-    //         return false;
-    //     }
-    // }
+        if (blocks.every(x => x.result) && block.content === '') {
+            return true; // need caller to remove
+        }
+        else {
+            for (const subBlock of blocks.filter(x => x.result).map(x => x.block)) {
+                await logseq.Editor.removeBlock(subBlock.uuid);
+            }
+            return false;
+        }
+    }
 
-    // logseq.beforeunload(
-    //     logseq.Editor.registerBlockContextMenuItem('clean empty pages', async (e) => {
-    //         const block = await logseq.Editor.getBlock(e.uuid, { includeChildren: true });
-    //         // if (block) {
-    //         //     await walk(block);
-    //         // }
-    //     }) as any
-    // );
+    logseq.beforeunload(
+        logseq.Editor.registerBlockContextMenuItem('clean empty pages', async (e) => {
+            const block = await logseq.Editor.getBlock(e.uuid, { includeChildren: true });
+            if (block) {
+                await walk(block);
+            }
+        }) as any
+    );
 
     logseq.beforeunload(
         logseq.App.registerPageMenuItem('clean empty pages', async ({ page }) => {
@@ -66,9 +53,9 @@ async function main() {
             const blocks = await logseq.Editor.getPageBlocksTree(page);
             if (blocks.length) {
                 for (const block of blocks) {
-                    // if (await walk(block)) {
-                    //     await logseq.Editor.removeBlock(block.uuid);
-                    // }
+                    if (await walk(block)) {
+                        await logseq.Editor.removeBlock(block.uuid);
+                    }
                 }
             }
         }) as any
